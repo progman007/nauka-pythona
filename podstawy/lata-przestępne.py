4516f9e0fce9ef8338c5c9a23750510a7e57b60a
progman007 <djgurgul@gmail.com>
2017-12-30 13:39:02 +0100

    Sprawdzanie czy rok jest przestępny

--- podstawy/lata-przestępne.py
+++ podstawy/lata-przestępne.py
@@ -1,2 +1,9 @@
-rok = input ("podaj rok")
-print (rok) 
+rok = int(input("podaj rok\n"))
+print(rok) 
+if rok % 400 == 0:
+	print("przestępny")
+elif rok % 4 == 0 and rok % 100 != 0:
+	print("przestępny")
+else:
+	print("nie jest przestępny")
+	
